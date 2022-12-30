9f237003dcd45a7954c84b29a1ba5d03cdc944eb
brunofelixf <brunofelixf@gmail.com>
2022-12-29 21:13:38 -0300

    fix: corrige importação do tratamento de erros

--- src/services/session/session.service.ts
+++ src/services/session/session.service.ts
@@ -1,18 +1,18 @@
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
-import { errorApp } from '../../errors/errorApp';
+import { ErrorApp } from '../../errors/errorApp';
 import { prisma } from './../../server';
 
 const authenticateUserService =
     async ( username: string, password: string ) => {
     const user = await prisma.user
         .findUniqueOrThrow({ where: {username} })
-        .catch( () => { throw new errorApp('Usuário não encontrado', 303)})
+        .catch( () => { throw new ErrorApp('Usuário não encontrado', 303)})
     
     const passwordIsCorrect = await compare( password, user.password );    
 
     if( !passwordIsCorrect ){
-        throw new errorApp('Username ou Senha inválidos', 401)
+        throw new ErrorApp('Username ou Senha inválidos', 401)
     }
 
     const token = sign(
