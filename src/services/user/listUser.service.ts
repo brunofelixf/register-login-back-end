957ec4f9dfeb58e7fd96ce418d518a28f7ed2f08
brunofelixf <brunofelixf@gmail.com>
2023-01-03 20:37:08 -0300

    refactor: corrige atributos de saída ao listar usuários

--- src/services/user/listUser.service.ts
+++ src/services/user/listUser.service.ts
@@ -5,8 +5,16 @@ import { prisma } from "../../server"
 const listUserService = async ( user_id: string | undefined ) => {
     const user = await prisma.user
         .findMany({
-            where: { user_id }
-        })
+            where: { user_id },
+            select: {
+                user_id: true,
+                name: true,
+                username: true,
+                email: true,
+                friend: true
+            }
+        },
+         )
         
         if( user.length == 0 ){
             throw new NotFoundError("Usuário não encontrado")
