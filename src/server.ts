5f2c4ea855db50a48eee43db8783ff02be4d3d5d
brunofelixf <brunofelixf@gmail.com>
2023-01-25 22:16:17 -0300

    chore: add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
-import {app} from './app'
-import { PrismaClient } from "@prisma/client";
+import { app } from './app'
+import { env } from 'process';
+import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient()
 
-const port = process.env.PORT || 3000
+const port = env.PORT || 3000
 
-app.listen( port, () => console.log( 'Server running (Port): ' + port ) )
+app.listen(port, () => { console.log('Server running (Port): ' + port) })
